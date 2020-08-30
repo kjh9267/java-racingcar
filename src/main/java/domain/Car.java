d3956ad46ca14718b980f8684d5d32277cb07561
kjh9267 <kjh9267z@naver.com>
2020-08-30 20:26:07 +0900

    refactor: Validation of car's name

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,24 @@
 package domain;
 
+import java.util.Objects;
+
 public class Car {
     private final String name;
     private final Engine engine;
 
     public Car(String name) {
+        validName(name);
         this.name = name;
         this.engine = new Engine();
+    }
+
+    private void validName(final String name) {
+        if (Objects.isNull(name) || name.isEmpty()) {
+            throw new IllegalArgumentException("이름은 null 이나 빈 값이 올 수 없습니다.");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하로 해주세요.");
+        }
     }
 
     public void move(final int value) {
--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class CarTest {
     private Car car;
@@ -27,6 +29,24 @@ public class CarTest {
         assertThat(car)
                 .isInstanceOf(Car.class)
                 .isEqualToComparingFieldByField(expected);
+    }
+
+    @DisplayName("이름이 null일 경우 예외 처리 하기.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void constructorTest2(String name) {
+        assertThatThrownBy(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름은 null 이나 빈 값이 올 수 없습니다.");
+    }
+
+
+    @DisplayName("이름이 5자가 넘을 경우 예외 처리 하기.")
+    @Test
+    void constructorTest3() {
+        assertThatThrownBy(() -> new Car("123456"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이름은 5자 이하로 해주세요.");
     }
 
     @DisplayName("여러 자동차가 같은 값을 받았을 때 똑같이 움직이는지 확인한다.")
