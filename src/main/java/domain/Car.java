7b6c406924c092cf42bd79852d0d99b0ff2dc585
hj <a301dks@naver.com>
2020-08-30 19:46:08 +0900

    feat: implement car move method - feat: implement findCurrentPosition method of car

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -3,14 +3,27 @@ package domain;
 public class Car {
     private final String name;
     private final Engine engine;
-    private final int position = 0;
 
     public Car(String name) {
         this.name = name;
         this.engine = new Engine();
     }
 
+    public void move(final int value) {
+        if (engine.canRunning(value)) {
+            engine.run();
+        }
+    }
+
+    public int findCurrentPosition() {
+        return engine.getRunningCount();
+    }
+
     public String getName() {
         return name;
+    }
+
+    public Engine getEngine() {
+        return engine;
     }
 }
--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -3,6 +3,11 @@ package domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -22,5 +27,29 @@ public class CarTest {
         assertThat(car)
                 .isInstanceOf(Car.class)
                 .isEqualToComparingFieldByField(expected);
+    }
+
+    @DisplayName("여러 자동차가 같은 값을 받았을 때 똑같이 움직이는지 확인한다.")
+    @Test
+    void moveTest() {
+        final Car k3 = new Car("k3");
+
+        car.move(9);
+        k3.move(9);
+        car.move(0);
+        k3.move(0);
+
+        assertThat(car.findCurrentPosition()).isEqualTo(k3.findCurrentPosition());
+    }
+
+    @DisplayName("2만큼 움직인 자동차의 현재 위치가 2인지 확인한다.")
+    @Test
+    void findCurrentPositionTest() {
+        car.move(9);
+        car.move(8);
+
+        final int currentPosition = car.findCurrentPosition();
+
+        assertThat(currentPosition).isEqualTo(2);
     }
 }
