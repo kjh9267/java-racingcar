41ffa0b02b9ebe2ce29704e8fd240f3ff52c8571
hj <a301dks@naver.com>
2020-08-30 17:01:59 +0900

    feat: 자동차를 생성하는 기능

---
+++ src/test/java/domain/CarTest.java
@@ -0,0 +1,18 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CarTest {
+    @DisplayName("이름을 가진 자동차가 정상적으로 생성 되는지 확인한다.")
+    @Test
+    void constructorTest() {
+        final Car expected = new Car("jun");
+
+        assertThat(new Car("jun"))
+                .isInstanceOf(Car.class)
+                .isEqualToComparingFieldByField(expected);
+    }
+}
